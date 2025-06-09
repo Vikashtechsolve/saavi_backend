5be9688aa57a0ca4f1c6c7f7c80507d126f69208
Arnav <arnavkul07@gmail.com>
2025-06-09 19:56:26 +0530

    cors issue testing

--- src/index.js
+++ src/index.js
@@ -21,19 +21,19 @@ const allowedOrigins = [
   'https://saavi-frontend-admin.vercel.app'
 ];
 app.use(cors(
-  {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  options:"*",
-  credentials: true
-}
+//   {
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   options:"*",
+//   credentials: true
+// }
 ));
 
 
